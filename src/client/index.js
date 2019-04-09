20a77fbc63b93f4318e81d3c4c0e892b0d071882
Seri-Lis <seri@lifeis.style>
2019-04-09 10:36:06 +0900

    feat:add Bufey

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable */
 const app = new Vue({
